e9814c633d96b9394fc45cc6a3178f01df1ba771
Kian Shirvan <spoono17@gmail.com>
2021-08-20 00:40:05 -0600

    update

--- index/express.js
+++ index/express.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.listen(process.env.PORT || port, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.listen(process.env.PORT, function(){
+    console.log("Express server listening on port %d in %s mode", process.env.PORT, app.settings.env);
 });
