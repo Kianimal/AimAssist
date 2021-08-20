6d41f86fb47bd5a08110b4c47dd1106d5c6772bd
Kian Shirvan <spoono17@gmail.com>
2021-08-20 13:51:37 -0600

    firebase update

--- express.js
+++ express.js
@@ -13,3 +13,16 @@ app.listen(process.env.PORT || port, () => {
 })
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCSuvRDAImIauW9r3Sy3VTUC_Wk_tolZUQ",
+  authDomain: "aimassist-350cf.firebaseapp.com",
+  projectId: "aimassist-350cf",
+  storageBucket: "aimassist-350cf.appspot.com",
+  messagingSenderId: "581818834530",
+  appId: "1:581818834530:web:c91dd8aedbd70c06c2bb3b",
+  measurementId: "G-GKVC569PTE"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
